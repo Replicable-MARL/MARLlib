preprocess model not gradient descent bug of maddpg fix for MetaDrive/MPE

MPE/model/torch_maddpg.py
@@ -69,9 +69,6 @@ class MADDPGTorchModel(TorchModelV2, nn.Module):
         super(MADDPGTorchModel, self).__init__(obs_space, action_space,
                                              num_outputs, model_config, name)
 
-        self.model = TorchFC(obs_space, action_space, num_outputs,
-                             model_config, name)
-
         self.bounded = np.logical_and(self.action_space.bounded_above,
                                       self.action_space.bounded_below).any()
         self.action_dim = np.product(self.action_space.shape)
@@ -234,6 +231,5 @@ class MADDPGTorchModel(TorchModelV2, nn.Module):
 
     @override(ModelV2)
     def forward(self, input_dict, state, seq_lens):
-        model_out, _ = self.model(input_dict, state, seq_lens)
-        return model_out, []
+        return input_dict["obs_flat"].float(), state
 

MPE/utils/maddpg_tools.py
@@ -229,14 +229,10 @@ def central_vf_stats_ddpg(policy, train_batch, grads):
 def build_maddpg_models(policy: Policy, observation_space: gym.spaces.Space,
                         action_space: gym.spaces.Space,
                         config: TrainerConfigDict) -> ModelV2:
-    if policy.config["use_state_preprocessor"]:
-        default_model = None  # catalog decides
-        num_outputs = 256  # arbitrary
-        config["model"]["no_final_linear"] = True
-    else:
-        default_model = TorchNoopModel if config["framework"] == "torch" \
-            else NoopModel
-        num_outputs = int(np.product(observation_space.shape))
+
+    default_model = TorchNoopModel if config["framework"] == "torch" \
+        else NoopModel
+    num_outputs = int(np.product(observation_space.shape))
 
     policy.model = ModelCatalog.get_model_v2(
         obs_space=observation_space,
@@ -326,6 +322,7 @@ def maddpg_centralized_critic_postprocessing(policy,
                                                     1)
         sample_batch["opponent_action"] = np.stack([opponent_batch[i]["actions"] for i in range(opponent_agents_num)],
                                                    1)
+
     else:
         # Policy hasn't been initialized yet, use zeros.
         sample_batch["opponent_obs"] = np.zeros(

MetaDrive/util/maddpg_tools.py
@@ -234,14 +234,10 @@ def central_vf_stats_ddpg(policy, train_batch, grads):
 def build_maddpg_models(policy: Policy, observation_space: gym.spaces.Space,
                         action_space: gym.spaces.Space,
                         config: TrainerConfigDict) -> ModelV2:
-    if policy.config["use_state_preprocessor"]:
-        default_model = None  # catalog decides
-        num_outputs = 256  # arbitrary
-        config["model"]["no_final_linear"] = True
-    else:
-        default_model = TorchNoopModel if config["framework"] == "torch" \
-            else NoopModel
-        num_outputs = int(np.product(observation_space.shape))
+
+    default_model = TorchNoopModel if config["framework"] == "torch" \
+        else NoopModel
+    num_outputs = int(np.product(observation_space.shape))
 
     policy.model = ModelCatalog.get_model_v2(
         obs_space=observation_space,