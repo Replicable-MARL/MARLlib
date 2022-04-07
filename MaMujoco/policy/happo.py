849ef3606047a49b09757a3a324ac1496bf03a65
fortymiles <minchiuan@zju.edu.cn>
2022-04-07 23:39:41 +0800

    add clip param -> 10

--- MaMujoco/policy/happo.py
+++ MaMujoco/policy/happo.py
@@ -33,12 +33,15 @@ def run_happo(args, common_config, env_config, stop):
     config.update(common_config)
 
     config.update({
+        "seed": 1,
         "env": args.map,
         "horizon": 1000,
         "num_sgd_iter": 5,  # ppo-epoch
         "train_batch_size": 4000,
         "sgd_minibatch_size": sgd_minibatch_size,
         "lr": 5e-5,
+        "grad_clip": 10,
+        "clip_param": 0.3,
         "model": {
             "custom_model": "{}_CentralizedCritic".format(args.neural_arch),
             "custom_model_config": {
