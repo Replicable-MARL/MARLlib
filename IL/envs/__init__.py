f84150aa977a26bb24bd30bf4a270bd509cfd504
Theohhhu <hhhusiyi@163.com>
2022-04-27 22:08:08 +1000

    IL env Pommerman

--- IL/envs/__init__.py
+++ IL/envs/__init__.py
@@ -6,8 +6,8 @@ from IL.envs.rware_rllib import RllibRWARE
 from IL.envs.lbf_rllib import RllibLBF
 from IL.envs.magent_rllib import RllibMAgent
 from IL.envs.metadrive_rllib import RllibMetaDrive
+from IL.envs.pommerman_rllib import RllibPommerman
 
-# cooperative only scenarios
 ENV_REGISTRY = {
     "mpe": RllibMPE,
     "rware": RllibRWARE,
@@ -16,5 +16,6 @@ ENV_REGISTRY = {
     "smac": RLlibSMAC,
     "football": RllibGFootball,
     "magent": RllibMAgent,
-    "metadrive": RllibMetaDrive
+    "metadrive": RllibMetaDrive,
+    "pommerman": RllibPommerman
 }
