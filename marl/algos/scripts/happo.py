65f037f80eefecc19e6de75b7ffc9894364607ff
fortymiles <minchiuan@zju.edu.cn>
2022-06-08 22:30:21 +0800

    add mappo and happo for fine tune scripts

--- marl/algos/scripts/happo.py
+++ marl/algos/scripts/happo.py
@@ -2,9 +2,12 @@ from ray import tune
 from ray.tune.utils import merge_dicts
 from ray.tune import CLIReporter
 from marl.algos.core.CC.happo import HAPPOTrainer
+from marl.algos.utils.setup_utils import _algos_var
 
 
 def run_happo(config_dict, common_config, env_dict, stop):
+    _param = _algos_var(config_dict)
+
     """
     for bug mentioned https://github.com/ray-project/ray/pull/20743
     make sure sgd_minibatch_size > max_seq_len
@@ -17,26 +20,36 @@ def run_happo(config_dict, common_config, env_dict, stop):
 
     algorithm = config_dict["algorithm"]
     batch_mode = config_dict["algo_args"]["batch_mode"]
-    lr = config_dict["algo_args"]["lr"]
+    # lr = config_dict["algo_args"]["lr"]
     iteration = config_dict["algo_args"]["iteration"]
     clip_param = config_dict["algo_args"]["clip_param"]
     vf_clip_param = config_dict["algo_args"]["vf_clip_param"]
     entropy_coeff = config_dict["algo_args"]["entropy_coeff"]
+    horizon = config_dict['algo_args']['horizon']
+    grad_clip = config_dict['algo_args']['grad_clip']
+    use_critic = config_dict['algo_args']['use_critic']
+    gamma = config_dict['algo_args']['gamma']
 
     config = {
-        "seed": tune.grid_search([1, 123]),
-        "batch_mode": batch_mode,
+        "seed": tune.grid_search([1]),
+        # "seed": 123,
+        # "batch_mode": batch_mode,
+        "horizon": horizon,
+        "num_sgd_iter": iteration,
         "train_batch_size": train_batch_size,
         "sgd_minibatch_size": sgd_minibatch_size,
-        "lr": lr,
-        "entropy_coeff": entropy_coeff,
-        "num_sgd_iter": iteration,
+        # "lr": _param('lr'),
+        # "entropy_coeff": entropy_coeff,
+        "grad_clip": grad_clip,
+        "use_critic": use_critic,
         "clip_param": clip_param,
-        "vf_clip_param": vf_clip_param,  # very sensitive, depends on the scale of the rewards
+        "gamma": gamma,
+        # "vf_clip_param": vf_clip_param,  # very sensitive, depends on the scale of the rewards
         "model": {
             "custom_model": "Centralized_Critic_Model",
-            "max_seq_len": episode_limit,
+            # "max_seq_len": episode_limit,
             "custom_model_config": merge_dicts(config_dict, env_dict),
+            "vf_share_layers": True,
         },
     }
     config.update(common_config)
