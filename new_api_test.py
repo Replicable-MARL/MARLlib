4469b0562d41be31dfbcdf6f8731f9d89a42c3d7
fortymiles <minchiuan@zju.edu.cn>
2023-01-09 00:39:55 -0800

    change to test mappo

--- new_api_test.py
+++ new_api_test.py
@@ -5,8 +5,8 @@ env = marl.make_env(environment_name='smac', map_name='3m', mask_flag=True, glob
 
 env.set_ray(local_model=True, stop_iters=100000)
 
-happo = marl.algos.HAPPO(use_gae=True, gamma=0.99, _lambda=1.0, batch_episode=10, kl_coeff=0.2, num_sgd_iter=5, grad_clip=10, clip_param=0.3, critic_lr=0.0005,
-                         vf_loss_coeff=1.0, lr=0.0000005, entropy_coeff=0.01, vf_clip_param=10.0,
+happo = marl.algos.MAPPO(use_gae=True,  _lambda=1.0, batch_episode=10, kl_coeff=0.2, num_sgd_iter=5, grad_clip=10, clip_param=0.3, critic_lr=0.0005,
+                         vf_loss_coeff=1.0, lr=0.0000005, entropy_coeff=0.01, vf_clip_param=10.0, gain=0.5,
                          batch_mode="complete_episodes")
 
 happo.fit_online(env, stop={'episode_reward_mean': 2000}, num_workers=4)
