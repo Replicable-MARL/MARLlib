16494e29c24c12958e85ea8f14c95f25a30a3c39
fortymiles <minchiuan@zju.edu.cn>
2023-03-11 12:37:23 -0800

    fix parameters when test happo

--- tests/algorithm/test_algorithm.py
+++ tests/algorithm/test_algorithm.py
@@ -7,20 +7,27 @@ class TestMPEEnv(unittest.TestCase):
         for algo_name in dir(marl.algos):
             if algo_name[:2] != "__":
                 if algo_name in ["ddpg", "maddpg", "facmac"]:
-                    env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True,
-                                        continuous_actions=True)
-                    algo = getattr(marl.algos, algo_name)(hyperparam_source="test")
-                    model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "16-16"})
-                    algo.fit(env, model, stop={"training_iteration": 3}, local_mode=False, num_gpus=0,
-                                 num_workers=2, share_policy="all", checkpoint_end=False)
-                elif algo_name in ["happo", "hatrpo"]:
                     continue
-                else:
-                    env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True)
+                    # env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True,
+                    #                     continuous_actions=True)
+                    # algo = getattr(marl.algos, algo_name)(hyperparam_source="test")
+                    # model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "16-16"})
+                    # algo.fit(env, model, stop={"training_iteration": 3}, local_mode=False, num_gpus=0,
+                    #              num_workers=2, share_policy="all", checkpoint_end=False)
+                elif algo_name in ["happo", "hatrpo"]:
+                    env = marl.make_env(environment_name="smac", map_name="3m")
                     algo = getattr(marl.algos, algo_name)(hyperparam_source="test")
-                    model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "16-16"})
-                    algo.fit(env, model, stop={"training_iteration": 3}, local_mode=False, num_gpus=0,
-                                 num_workers=2, share_policy="all", checkpoint_end=False)
+                    model = marl.build_model(env, algo, {"core_arch": "gru", "encode_layer": "16-16"})
+                    algo.fit(env, model, stop={"training_iteration": 3}, local_mode=True, num_gpus=0,
+                                 num_workers=2, share_policy="individual", checkpoint_end=False)
+
+                else:
+                    continue
+                    # env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True)
+                    # algo = getattr(marl.algos, algo_name)(hyperparam_source="test")
+                    # model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "16-16"})
+                    # algo.fit(env, model, stop={"training_iteration": 3}, local_mode=False, num_gpus=0,
+                    #              num_workers=2, share_policy="all", checkpoint_end=False)
 
 
 
