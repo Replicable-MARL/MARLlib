df063f12fc9e595804a15b40d8e4d89f1809db01
fortymiles <minchiuan@zju.edu.cn>
2023-05-22 22:34:03 -0700

    rm uninstalled envs

--- tests/test_env.py
+++ tests/test_env.py
@@ -38,96 +38,96 @@ class TestBaseOnMAPPO(unittest.TestCase):
         algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
                  num_workers=2, share_policy="group", checkpoint_end=False)
 
-    def test_b1_magent(self):
-        env = marl.make_env(environment_name="magent", map_name="gather")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
+    # def test_b1_magent(self):
+    #     env = marl.make_env(environment_name="magent", map_name="gather")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_c1_smac(self):
+    #     env = marl.make_env(environment_name="smac", map_name="3m")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_d1_grf(self):
+    #     env = marl.make_env(environment_name="football", map_name="academy_pass_and_shoot_with_keeper")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
 
-    def test_c1_smac(self):
-        env = marl.make_env(environment_name="smac", map_name="3m")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_d1_grf(self):
-        env = marl.make_env(environment_name="football", map_name="academy_pass_and_shoot_with_keeper")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_e1_rware(self):
-        env = marl.make_env(environment_name="rware", map_name="rware")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp","encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_f1_lbf(self):
-        env = marl.make_env(environment_name="lbf", map_name="lbf")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_g1_pommerman(self):
-        env = marl.make_env(environment_name="pommerman", map_name="PommeTeamCompetition-v0")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_h1_metadrive(self):
-        env = marl.make_env(environment_name="metadrive", map_name="Bottleneck")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=False, num_gpus=1,
-                 num_workers=3, share_policy="group", checkpoint_end=False)
-
-    def test_i1_hanabi(self):
-        env = marl.make_env(environment_name="hanabi", map_name="Hanabi-Very-Small")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_j1_mate(self):
-        env = marl.make_env(environment_name="mate", map_name="MATE-4v2-9-v0")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_k1_gobigger(self):
-        env = marl.make_env(environment_name="gobigger", map_name="st_t1p2")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_l1_overcooked(self):
-        env = marl.make_env(environment_name="overcooked", map_name="asymmetric_advantages")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_m1_voltage(self):
-        env = marl.make_env(environment_name="voltage", map_name="case33_3min_final")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
-
-    def test_n1_mamujoco(self):
-        env = marl.make_env(environment_name="mamujoco", map_name="2AgentAnt")
-        algo = marl.algos.mappo(hyperparam_source="test")
-        model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
-        algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
-                 num_workers=2, share_policy="group", checkpoint_end=False)
+    # def test_e1_rware(self):
+    #     env = marl.make_env(environment_name="rware", map_name="rware")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp","encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_f1_lbf(self):
+    #     env = marl.make_env(environment_name="lbf", map_name="lbf")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_g1_pommerman(self):
+    #     env = marl.make_env(environment_name="pommerman", map_name="PommeTeamCompetition-v0")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_h1_metadrive(self):
+    #     env = marl.make_env(environment_name="metadrive", map_name="Bottleneck")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=False, num_gpus=1,
+    #              num_workers=3, share_policy="group", checkpoint_end=False)
+    #
+    # def test_i1_hanabi(self):
+    #     env = marl.make_env(environment_name="hanabi", map_name="Hanabi-Very-Small")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_j1_mate(self):
+    #     env = marl.make_env(environment_name="mate", map_name="MATE-4v2-9-v0")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_k1_gobigger(self):
+    #     env = marl.make_env(environment_name="gobigger", map_name="st_t1p2")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_l1_overcooked(self):
+    #     env = marl.make_env(environment_name="overcooked", map_name="asymmetric_advantages")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_m1_voltage(self):
+    #     env = marl.make_env(environment_name="voltage", map_name="case33_3min_final")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
+    #
+    # def test_n1_mamujoco(self):
+    #     env = marl.make_env(environment_name="mamujoco", map_name="2AgentAnt")
+    #     algo = marl.algos.mappo(hyperparam_source="test")
+    #     model = marl.build_model(env, algo, {"core_arch": "mlp", "encode_layer": "8-8"})
+    #     algo.fit(env, model, stop={"training_iteration": 1}, local_mode=True, num_gpus=0,
+    #              num_workers=2, share_policy="group", checkpoint_end=False)
 
 
 if __name__ == "__main__":
